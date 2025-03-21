bfb2625a2822e8607b4bed3706c587c4976f030e
Kevin Hannon <kehannon@redhat.com>
2025-03-21 15:44:07 -0400

    add jobset support into kjob

--- pkg/cmd/list/list.go
+++ pkg/cmd/list/list.go
@@ -41,6 +41,7 @@ func NewListCmd(clientGetter util.ClientGetter, streams genericiooptions.IOStrea
 	}
 
 	cmd.AddCommand(NewJobCmd(clientGetter, streams, clock))
+	cmd.AddCommand(NewJobSetCmd(clientGetter, streams, clock))
 	cmd.AddCommand(NewInteractiveCmd(clientGetter, streams, clock))
 	cmd.AddCommand(NewRayJobCmd(clientGetter, streams, clock))
 	cmd.AddCommand(NewRayClusterCmd(clientGetter, streams, clock))
