b91c33de71f2b9d9d26b87910c110e919aa7bcdf
Kevin Hannon <kehannon@redhat.com>
2025-07-13 16:46:17 -0400

    address feedback

--- pkg/cmd/list/list_jobset_test.go
+++ pkg/cmd/list/list_jobset_test.go
@@ -1,5 +1,5 @@
 /*
-Copyright %s Apache License, Version 2.0 (the "License");
+Copyright 2025 Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
